0c60e8ac45f0713a07051c00f6de73622499d3ba
Mugshot1981 <joasdewit@gmail.com>
2026-03-10 20:13:01 +0100

    Update courseCatalog.js

--- js/data/courseCatalog.js
+++ js/data/courseCatalog.js
@@ -1,3 +1,8 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
 import { historyMavo4Course } from "./courses/geschiedenis-mavo-4.js";
 import { historyHavo3Course } from "./courses/geschiedenis-havo-3.js";
 import { aardrijkskundeMavo4Course } from "./courses/aardrijkskunde-mavo-4.js";
