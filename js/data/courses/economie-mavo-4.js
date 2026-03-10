247f03bb21745bb961836783a76dede809974b54
Mugshot1981 <joasdewit@gmail.com>
2026-03-10 18:26:09 +0100

    Create economie-mavo-4.js

---
+++ js/data/courses/economie-mavo-4.js
@@ -0,0 +1,42 @@
+chapters: [
+  {
+    id: "eco-h1",
+    subjectId: "economie",
+    title: "Inkomen en welvaart"
+  },
+  {
+    id: "eco-h2",
+    subjectId: "economie",
+    title: "Geld genoeg?"
+  },
+  {
+    id: "eco-h3",
+    subjectId: "economie",
+    title: "Ben jij ondernemend?"
+  },
+  {
+    id: "eco-h4",
+    subjectId: "economie",
+    title: "Werk aan de winkel!"
+  },
+  {
+    id: "eco-h5",
+    subjectId: "economie",
+    title: "Hoe werkt de overheid?"
+  },
+  {
+    id: "eco-h6",
+    subjectId: "economie",
+    title: "Iedereen betaalt belasting"
+  },
+  {
+    id: "eco-h7",
+    subjectId: "economie",
+    title: "Nederland en het buitenland"
+  },
+  {
+    id: "eco-h8",
+    subjectId: "economie",
+    title: "Ontwikkelingslanden"
+  }
+],
