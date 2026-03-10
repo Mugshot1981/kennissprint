4877f228d37f998f65aa816182c6c6635671611b
Mugshot1981 <joasdewit@gmail.com>
2026-03-10 17:54:24 +0100

    Update aardrijkskunde-mavo-4.js

--- js/data/courses/aardrijkskunde-mavo-4.js
+++ js/data/courses/aardrijkskunde-mavo-4.js
@@ -14,7 +14,200 @@ export const aardrijkskundeMavo4Course = {
   ],
 
   datasets: {
-    terms: [],
+    terms: [
+      {
+term: "frontale regen",
+definition: "regen die ontstaat doordat warme lucht over koude lucht opstijgt wanneer het condenseert"
+},
+{
+term: "hogedrukgebied",
+definition: "een gebied met een hoge luchtdruk door dalende lucht"
+},
+{
+term: "lagedrukgebied",
+definition: "een gebied met een lage luchtdruk waar de lucht stijgt"
+},
+{
+term: "landwind",
+definition: "een wind die vanaf het land naar de zee waait"
+},
+{
+term: "lijzijde",
+definition: "de kant van een gebergte waar de droge lucht naar beneden komt"
+},
+{
+term: "loefzijde",
+definition: "de kant van een gebergte waar de lucht opstijgt condenseert en er neerslag valt"
+},
+{
+term: "neerslag",
+definition: "regen hagel sneeuw mist en ijzel"
+},
+{
+term: "regenschaduw",
+definition: "de kant van een gebergte waar de droge lucht naar beneden komt"
+},
+{
+term: "schaal van Beaufort",
+definition: "de schaal van Beaufort waarmee windkracht wordt gemeten van windstil tot orkaankracht"
+},
+{
+term: "stijgingsregen",
+definition: "regen die ontstaat als warme lucht opstijgt en daardoor afkoelt"
+},
+{
+term: "stuwingsregen",
+definition: "regen die ontstaat wanneer lucht gedwongen wordt tegen een berghelling op te stijgen"
+},
+{
+term: "temperatuur",
+definition: "hoe warm of koud het is gemeten in graden"
+},
+{
+term: "waterkringloop",
+definition: "het proces waarbij water via verdamping wolken neerslag grondwater en rivieren terugstroomt naar zee"
+},
+{
+term: "weer",
+definition: "de toestand van de luchtlaag om de aarde op een bepaald moment en plaats"
+},
+{
+term: "weerselementen",
+definition: "neerslag temperatuur en wind"
+},
+{
+term: "wind",
+definition: "verplaatsing van lucht van een hogedrukgebied naar een lagedrukgebied"
+},
+{
+term: "zeewind",
+definition: "een wind die vanuit zee naar het land waait"
+},
+{
+term: "breedteligging",
+definition: "de geografische ligging van een plaats ten opzichte van de evenaar uitgedrukt in graden"
+},
+{
+term: "hoogteligging",
+definition: "de ligging van een gebied uitgedrukt in meters boven zeespiegel"
+},
+{
+term: "klimaat",
+definition: "het gemiddelde weer gemeten over een periode van dertig jaar"
+},
+{
+term: "klimaatfactoren",
+definition: "breedteligging hoogteligging afstand tot zee type oppervlak en aanvoer van lucht"
+},
+{
+term: "landklimaat",
+definition: "klimaat met een groot verschil tussen zomer en wintertemperatuur"
+},
+{
+term: "poolklimaat",
+definition: "klimaat waarbij de temperatuur altijd onder de 0 graden Celsius blijft"
+},
+{
+term: "toendraklimaat",
+definition: "klimaat waarbij zelfs in de zomer de temperatuur niet boven de 10 graden komt"
+},
+{
+term: "tropisch klimaat",
+definition: "klimaat met een maandtemperatuur boven de 18 graden en het hele jaar veel regen"
+},
+{
+term: "woestijnklimaat",
+definition: "klimaat met meestal hoge temperaturen en vrijwel geen neerslag"
+},
+{
+term: "broeikasgassen",
+definition: "gassen die bijdragen aan het opwarmen van de aarde"
+},
+{
+term: "duurzaamheid",
+definition: "voorzien in de eigen behoefte zonder de leefbaarheid voor toekomstige generaties te schaden"
+},
+{
+term: "methaan",
+definition: "een broeikasgas dat vrijkomt bij koeien en moerassen"
+},
+{
+term: "natuurlijk broeikaseffect",
+definition: "de dampkring houdt warmte vast die de aarde uitstraalt nadat de zon de aarde heeft verwarmd"
+},
+{
+term: "versterkt broeikaseffect",
+definition: "mensen brengen te veel broeikasgassen in de atmosfeer waardoor de aarde warmer wordt"
+},
+{
+term: "globale windsysteem",
+definition: "de manier waarop lucht over de aarde wordt verdeeld"
+},
+{
+term: "klimaatgrafiek",
+definition: "grafiek met de temperatuur en neerslag van een plaats gedurende het jaar"
+},
+{
+term: "neerslagintensiteit",
+definition: "de hoeveelheid neerslag die valt per tijdseenheid"
+},
+{
+term: "neerslagverdeling",
+definition: "de verdeling van neerslag over het jaar"
+},
+{
+term: "gematigd zeeklimaat",
+definition: "klimaat waarin het verschil tussen zomer en winter klein is en het hele jaar neerslag valt"
+},
+{
+term: "hooggebergteklimaat",
+definition: "een sneeuw en ijsklimaat hoog in de bergen"
+},
+{
+term: "mediterraan klimaat",
+definition: "gematigd klimaat met droge zomers en zachte winters met neerslag"
+},
+{
+term: "steppeklimaat",
+definition: "klimaat met weinig neerslag en een begroeiing van gras"
+},
+{
+term: "draineren",
+definition: "het afvoeren van water met behulp van buizen"
+},
+{
+term: "extensieve landbouw",
+definition: "landbouw waarbij weinig arbeid of kapitaal wordt gebruikt maar veel grond"
+},
+{
+term: "intensieve landbouw",
+definition: "landbouw waarbij veel arbeid of kapitaal per hectare wordt gebruikt"
+},
+{
+term: "irrigeren",
+definition: "kunstmatige bevloeiing van landbouwgebieden"
+},
+{
+term: "landdegradatie",
+definition: "achteruitgang van de kwaliteit van land door erosie uitdroging ontbossing of uitputting"
+},
+{
+term: "vormen van irrigatie",
+definition: "bevloeien van akkers door middel van besproeien kanaaltjes of druppelirrigatie"
+},
+{
+term: "waterbalans",
+definition: "verschil tussen neerslag verdamping en aan en afvoer van water in een gebied"
+},
+{
+term: "klimaatafspraken",
+definition: "afspraken tussen landen om de hoeveelheid CO2 in de atmosfeer te verminderen"
+},
+{
+term: "uv straling",
+definition: "ultraviolette straling van de zon die je niet kunt zien"
+}
+    ],
     years: [],
     persons: []
   },
