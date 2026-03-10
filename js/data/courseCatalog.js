1ffdfd330629f93ca112c6944f80b5191b57fa80
Mugshot1981 <joasdewit@gmail.com>
2026-03-10 18:29:06 +0100

    Update courseCatalog.js

--- js/data/courseCatalog.js
+++ js/data/courseCatalog.js
@@ -1,10 +1,11 @@
-
 import { historyMavo4Course } from "./courses/geschiedenis-mavo-4.js";
 import { historyHavo3Course } from "./courses/geschiedenis-havo-3.js";
 import { aardrijkskundeMavo4Course } from "./courses/aardrijkskunde-mavo-4.js";
+import { economieMavo4Course } from "./courses/economie-mavo-4.js";
 
 export const courseCatalog = [
   historyMavo4Course,
   historyHavo3Course,
-  aardrijkskundeMavo4Course
+  aardrijkskundeMavo4Course,
+  economieMavo4Course
 ];
