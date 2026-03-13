3188a2bc6a4b7ffb4c27954b6b104c1b95812f27
Mugshot1981 <joasdewit@gmail.com>
2026-03-13 14:52:01 +0100

    Update duits-mavo-4.js

--- js/data/courses/duits-mavo-4.js
+++ js/data/courses/duits-mavo-4.js
@@ -1,1 +1,39 @@
 
+export const duitsMavo4Course = {
+
+  id: "duits-mavo-4",
+  subject: "duits",
+  level: "mavo",
+  grade: "4",
+  title: "Duits 4 mavo",
+
+  chapters: [
+
+    { id: "du-h1", title: "Essen und Trinken" },
+    { id: "du-h2", title: "Körper" },
+    { id: "du-h3", title: "Freizeit" },
+    { id: "du-h4", title: "Menschen" },
+    { id: "du-h5", title: "Schule" },
+    { id: "du-h6", title: "Arbeit" },
+    { id: "du-h7", title: "Gefühle" },
+    { id: "du-h8", title: "Kunst und Kultur" },
+    { id: "du-h9", title: "Tourismus" },
+    { id: "du-h10", title: "Umgebung" },
+    { id: "du-h11", title: "Kontakte" },
+    { id: "du-h12", title: "Innovation" },
+    { id: "du-h13", title: "Geld" },
+    { id: "du-h14", title: "Umwelt" },
+    { id: "du-h15", title: "Nachhaltigkeit" },
+    { id: "du-h16", title: "Gemeinschaftskunde" },
+    { id: "du-h17", title: "Falsche Freunde" },
+    { id: "du-h18", title: "Signalwörter" },
+    { id: "du-h19", title: "Prüfungssprache" },
+    { id: "du-h20", title: "Redemittel" }
+
+  ],
+
+  datasets: {
+    terms: []
+  }
+
+};
