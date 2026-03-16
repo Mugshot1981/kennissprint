fd095849233ced880fc02040a956f14e4f6af49a
Mugshot1981 <joasdewit@gmail.com>
2026-03-16 10:12:23 +0100

    Create tekenen-mavo-4.js

---
+++ js/data/courses/tekenen-mavo-4.js
@@ -0,0 +1,24 @@
+export const tekenenMavo4Course = {
+  id: "tekenen-mavo-4",
+  subject: "tekenen",
+  level: "mavo",
+  grade: "4",
+  title: "Tekenen 4 mavo",
+
+  chapters: [
+    {
+      id: "te-1",
+      title: "Beeldende begrippen"
+    },
+    {
+      id: "te-2",
+      title: "Kunstgeschiedenis"
+    }
+  ],
+
+  datasets: {
+    terms: [],
+    persons: [],
+    years: []
+  }
+};
