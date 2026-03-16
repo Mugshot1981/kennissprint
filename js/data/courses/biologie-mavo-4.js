25621e120cab52eff081f171a53695d9fa30b93a
Mugshot1981 <joasdewit@gmail.com>
2026-03-16 10:12:44 +0100

    Create biologie-mavo-4.js

---
+++ js/data/courses/biologie-mavo-4.js
@@ -0,0 +1,18 @@
+export const biologieMavo4Course = {
+  id: "biologie-mavo-4",
+  subject: "biologie",
+  level: "mavo",
+  grade: "4",
+  title: "Biologie 4 mavo",
+
+  chapters: [
+    { id: "bio-1", title: "Cellen" },
+    { id: "bio-2", title: "Organen" }
+  ],
+
+  datasets: {
+    terms: [],
+    persons: [],
+    years: []
+  }
+};
