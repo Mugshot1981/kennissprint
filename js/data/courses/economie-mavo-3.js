6473931173cc81b7491a42945843c8044cc531ce
Mugshot1981 <joasdewit@gmail.com>
2026-03-17 17:00:45 +0100

    Create economie-mavo-3.js

---
+++ js/data/courses/economie-mavo-3.js
@@ -0,0 +1,59 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+export const economieMavo3Course = {
+  id: "economie-mavo-3",
+  subject: "economie",
+  level: "mavo",
+  grade: "3",
+  title: "Economie 3 mavo",
+
+  chapters: [
+    {
+      id: "eco3-h1",
+      subjectId: "economie",
+      title: "Hoofdstuk 1"
+    },
+    {
+      id: "eco3-h2",
+      subjectId: "economie",
+      title: "Hoofdstuk 2"
+    },
+    {
+      id: "eco3-h3",
+      subjectId: "economie",
+      title: "Hoofdstuk 3"
+    },
+    {
+      id: "eco3-h4",
+      subjectId: "economie",
+      title: "Hoofdstuk 4"
+    },
+    {
+      id: "eco3-h5",
+      subjectId: "economie",
+      title: "Hoofdstuk 5"
+    },
+    {
+      id: "eco3-h6",
+      subjectId: "economie",
+      title: "Hoofdstuk 6"
+    },
+    {
+      id: "eco3-h7",
+      subjectId: "economie",
+      title: "Hoofdstuk 7"
+    },
+    {
+      id: "eco3-h8",
+      subjectId: "economie",
+      title: "Hoofdstuk 8"
+    }
+  ],
+
+  datasets: {
+    terms: []
+  }
+};
