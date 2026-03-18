b41eb972a34f8a41266722f8bbacbdce84da7265
Mugshot1981 <joasdewit@gmail.com>
2026-03-18 12:35:37 +0100

    Update aardrijkskunde-vwo-2.js

--- js/data/courses/aardrijkskunde-vwo-2.js
+++ js/data/courses/aardrijkskunde-vwo-2.js
@@ -1,1 +1,20 @@
 
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { aardrijkskundeKlas2Shared } from "./aardrijkskunde-klas-2-shared.js";
+
+export const aardrijkskundeVwo2Course = {
+  id: "aardrijkskunde-vwo-2",
+  subject: "aardrijkskunde",
+  level: "vwo",
+  grade: "2",
+  title: "Aardrijkskunde vwo 2",
+
+  chapters: aardrijkskundeKlas2Shared.chapters,
+  datasets: aardrijkskundeKlas2Shared.datasets,
+  modes: aardrijkskundeKlas2Shared.modes
+};
