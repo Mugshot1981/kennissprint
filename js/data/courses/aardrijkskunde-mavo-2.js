9bfea3a5bb66d4acb2ea916cb827ef9066ecf7a5
Mugshot1981 <joasdewit@gmail.com>
2026-03-18 12:34:46 +0100

    Create aardrijkskunde-mavo-2.js

---
+++ js/data/courses/aardrijkskunde-mavo-2.js
@@ -0,0 +1,19 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { aardrijkskundeKlas2Shared } from "./aardrijkskunde-klas-2-shared.js";
+
+export const aardrijkskundeMavo2Course = {
+  id: "aardrijkskunde-mavo-2",
+  subject: "aardrijkskunde",
+  level: "mavo",
+  grade: "2",
+  title: "Aardrijkskunde mavo 2",
+
+  chapters: aardrijkskundeKlas2Shared.chapters,
+  datasets: aardrijkskundeKlas2Shared.datasets,
+  modes: aardrijkskundeKlas2Shared.modes
+};
