16a7d43d5392bcef942288a12b329b09e76dcdfb
Mugshot1981 <joasdewit@gmail.com>
2026-03-20 11:02:07 +0100

    Update geschiedenis-havo-2.js

--- js/data/courses/geschiedenis-havo-2.js
+++ js/data/courses/geschiedenis-havo-2.js
@@ -1,1 +1,13 @@
+import { geschiedenisKlas2Shared } from "./geschiedenis-klas-2-shared.js";
 
+export const historyHavo2Course = {
+  id: "geschiedenis-havo-2",
+  subject: "geschiedenis",
+  level: "havo",
+  grade: "2",
+  title: "Geschiedenis 2 havo",
+
+  chapters: geschiedenisKlas2Shared.chapters,
+  datasets: geschiedenisKlas2Shared.datasets,
+  modes: geschiedenisKlas2Shared.modes
+};
