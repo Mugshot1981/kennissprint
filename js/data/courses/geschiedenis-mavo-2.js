3f6093cd474c360c19d7a77fac3ce7910b4b752c
Mugshot1981 <joasdewit@gmail.com>
2026-03-20 11:01:51 +0100

    Update geschiedenis-mavo-2.js

--- js/data/courses/geschiedenis-mavo-2.js
+++ js/data/courses/geschiedenis-mavo-2.js
@@ -1,1 +1,14 @@
 
+import { geschiedenisKlas2Shared } from "./geschiedenis-klas-2-shared.js";
+
+export const historyMavo2Course = {
+  id: "geschiedenis-mavo-2",
+  subject: "geschiedenis",
+  level: "mavo",
+  grade: "2",
+  title: "Geschiedenis 2 mavo",
+
+  chapters: geschiedenisKlas2Shared.chapters,
+  datasets: geschiedenisKlas2Shared.datasets,
+  modes: geschiedenisKlas2Shared.modes
+};
