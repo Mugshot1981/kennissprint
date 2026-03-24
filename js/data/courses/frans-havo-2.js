082ae34dc31c0d3aecfc28acd19952d82f6a0726
Mugshot1981 <joasdewit@gmail.com>
2026-03-24 09:27:10 +0100

    Create frans-havo-2.js

---
+++ js/data/courses/frans-havo-2.js
@@ -0,0 +1,19 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { fransKlas2Shared } from "./frans-klas-2-shared.js";
+
+export const fransHavo2Course = {
+  id: "frans-havo-2",
+  subject: "frans",
+  level: "havo",
+  grade: "2",
+  title: "Frans havo 2",
+
+  chapters: fransKlas2Shared.chapters,
+  datasets: fransKlas2Shared.datasets,
+  modes: fransKlas2Shared.modes
+};
