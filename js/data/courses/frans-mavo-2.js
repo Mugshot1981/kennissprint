a72a0868d78617803b7d4d888c4949906bb18f4d
Mugshot1981 <joasdewit@gmail.com>
2026-03-24 09:28:01 +0100

    Create frans-mavo-2.js

---
+++ js/data/courses/frans-mavo-2.js
@@ -0,0 +1,19 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { fransKlas2Shared } from "./frans-klas-2-shared.js";
+
+export const fransMavo2Course = {
+  id: "frans-mavo-2",
+  subject: "frans",
+  level: "mavo",
+  grade: "2",
+  title: "Frans mavo 2",
+
+  chapters: fransKlas2Shared.chapters,
+  datasets: fransKlas2Shared.datasets,
+  modes: fransKlas2Shared.modes
+};
