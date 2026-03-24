f1f29c02d2960ae7a25cf1a859691eb6f58683c7
Mugshot1981 <joasdewit@gmail.com>
2026-03-24 09:26:42 +0100

    Update frans-klas-2-shared.js

--- js/data/courses/frans-klas-2-shared.js
+++ js/data/courses/frans-klas-2-shared.js
@@ -1,1 +1,82 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
 
+export const fransKlas2Shared = {
+  chapters: [
+    {
+      id: "fr-h1",
+      subjectId: "frans",
+      title: "Un, deux, splash!"
+    },
+    {
+      id: "fr-h2",
+      subjectId: "frans",
+      title: "Tu viens chez moi?"
+    },
+    {
+      id: "fr-h3",
+      subjectId: "frans",
+      title: "Les bonnes affaires"
+    },
+    {
+      id: "fr-h4",
+      subjectId: "frans",
+      title: "Le pont"
+    },
+    {
+      id: "fr-h5",
+      subjectId: "frans",
+      title: "Objectif santé"
+    },
+    {
+      id: "fr-h6",
+      subjectId: "frans",
+      title: "J’aime le camping!"
+    },
+    {
+      id: "fr-h7",
+      subjectId: "frans",
+      title: "Vive la fête!"
+    },
+    {
+      id: "fr-h8",
+      subjectId: "frans",
+      title: "Le pont"
+    }
+  ],
+
+  datasets: {
+    terms: [
+    ],
+    phrases: [
+    ],
+    years: [],
+    persons: []
+  },
+
+  modes: [
+    {
+      id: "terms-fr-nl",
+      label: "Frans → Nederlands",
+      dataset: "terms"
+    },
+    {
+      id: "terms-nl-fr",
+      label: "Nederlands → Frans",
+      dataset: "terms"
+    },
+    {
+      id: "phrases-nl-fr",
+      label: "Zinnen Nederlands → Frans",
+      dataset: "phrases"
+    },
+    {
+      id: "phrases-fr-nl",
+      label: "Zinnen Frans → Nederlands",
+      dataset: "phrases"
+    }
+  ]
+};
