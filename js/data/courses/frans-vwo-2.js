ec89aeb78f547a73e0a1b2e3653ccb90e5f93b67
Mugshot1981 <joasdewit@gmail.com>
2026-03-24 09:27:26 +0100

    Create frans-vwo-2.js

---
+++ js/data/courses/frans-vwo-2.js
@@ -0,0 +1,19 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { fransKlas2Shared } from "./frans-klas-2-shared.js";
+
+export const fransVwo2Course = {
+  id: "frans-vwo-2",
+  subject: "frans",
+  level: "vwo",
+  grade: "2",
+  title: "Frans vwo 2",
+
+  chapters: fransKlas2Shared.chapters,
+  datasets: fransKlas2Shared.datasets,
+  modes: fransKlas2Shared.modes
+};
