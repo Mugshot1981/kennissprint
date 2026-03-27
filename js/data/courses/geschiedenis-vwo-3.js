1812a019f2145bbae7c3901c6ab15e847f564382
Mugshot1981 <joasdewit@gmail.com>
2026-03-27 14:02:10 +0100

    Create geschiedenis-vwo-3.js

---
+++ js/data/courses/geschiedenis-vwo-3.js
@@ -0,0 +1,19 @@
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+import { geschiedenisHavo3Shared } from "./geschiedenis-havo-3-shared.js";
+
+export const historyVwo3Course = {
+  id: "geschiedenis-vwo-3",
+  subject: "geschiedenis",
+  level: "vwo",
+  grade: "3",
+  title: "Geschiedenis 3 vwo",
+
+  chapters: geschiedenisHavo3Shared.chapters,
+  datasets: geschiedenisHavo3Shared.datasets,
+  modes: geschiedenisHavo3Shared.modes
+};
