f577364fbe4f87139eeccfc81f1ef29277a84de0
Mugshot1981 <joasdewit@gmail.com>
2026-03-27 09:54:22 +0100

    Update engels-havo-2.js

--- js/data/courses/engels-havo-2.js
+++ js/data/courses/engels-havo-2.js
@@ -1,1 +1,13 @@
 
+import { english2Shared } from "./engels-2-shared.js";
+
+export const englishHavo2Course = {
+  id: "engels-havo-2",
+  subject: "engels",
+  level: "havo",
+  grade: "2",
+  title: "Engels 2 havo",
+  chapters: english2Shared.chapters,
+  datasets: english2Shared.datasets,
+  modes: english2Shared.modes
+};
