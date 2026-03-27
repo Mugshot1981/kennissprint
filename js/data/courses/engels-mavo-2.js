0f5ac41f12e2d5451d8e60b2d15cf8821d80ff65
Mugshot1981 <joasdewit@gmail.com>
2026-03-27 09:54:07 +0100

    Update engels-mavo-2.js

--- js/data/courses/engels-mavo-2.js
+++ js/data/courses/engels-mavo-2.js
@@ -1,1 +1,13 @@
 
+import { english2Shared } from "./engels-2-shared.js";
+
+export const englishMavo2Course = {
+  id: "engels-mavo-2",
+  subject: "engels",
+  level: "mavo",
+  grade: "2",
+  title: "Engels 2 mavo",
+  chapters: english2Shared.chapters,
+  datasets: english2Shared.datasets,
+  modes: english2Shared.modes
+};
